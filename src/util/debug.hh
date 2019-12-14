ccc9e67846ccf29963ccca5987ef9aa1ca0f1413
Christopher Arndt <chris@chrisarndt.de>
2019-12-14 18:39:56 +0100

    Update copyright year in all files to year of their latest commit
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- src/util/debug.hh
+++ src/util/debug.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2008  Dominic Sacré  <dominic.sacre@gmx.de>
+ * Copyright (C) 2007-2013  Dominic Sacré  <dominic.sacre@gmx.de>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
